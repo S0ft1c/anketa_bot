6e78e4efea17daec2e2f90c2b7df43314c93b191
user <stephandolskii@ya.ru>
2024-07-26 13:12:04 +0300

    created a view of exact order in workers menu

--- app/ispolnitel_handlers/inwork_handlers/__init__.py
+++ app/ispolnitel_handlers/inwork_handlers/__init__.py
@@ -2,11 +2,13 @@ from aiogram import Router
 
 from .active_work_handler import active_work_router
 from .join_to_order_request_handler import join_order_router
+from .worker_look_in_orderid_handler import worker_look_in_orderid_router
 
 inwork_router = Router()
 
 # include all routers
 inwork_router.include_routers(
     join_order_router,
-    active_work_router
+    active_work_router,
+    worker_look_in_orderid_router
 )
